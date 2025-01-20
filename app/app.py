ae6b678118dd11cbb69dceb5a13532e1c496b899
jacksonisidor <jax.isidor@gmail.com>
2025-01-20 11:31:57 -0800

    input and output working

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 # Read in data
 data = pd.read_parquet("data/all_matchup_stats.parquet")
 
+print('RETRIEVED GAME DATA')
 
 # Define a route for the home page
 @app.route('/')
@@ -21,15 +22,21 @@ def simulate():
         # get the json input from the request
         input_data = request.json
 
+        print("RETRIEVED INPUT DATA")
+
         # extract the selected parameters for the simulation (with default values)
         year = input_data.get('year', 2024)
         picked_winner = input_data.get('picked_winner')
         playstyle = input_data.get('playstyle', 'Balanced')
         boldness = input_data.get('boldness', 'Normal')
 
+        print("EXTRACTED PARAMETERS")
+
         # initialize and run the simulator
         simulator = BracketSimulator(data, year, picked_winner, playstyle, boldness)
         simulated_bracket = simulator.sim_bracket()
+
+        print("SIMULATED BRACKET")
 
         # return a placeholder response for now
         return jsonify({
@@ -42,6 +49,7 @@ def simulate():
             'results': simulated_bracket.head().to_dict(orient='records')
         })
     except Exception as e:
+        print("Error", str(e))
         return jsonify({
             'status': 'error',
             'message': str(e)
